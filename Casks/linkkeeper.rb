ff5e97e56b4785b919ae2d3473f48f0300c00dc8
shinya-ohtani-phd <ohtani.shinya@jp.panasonic.com>
2026-05-13 10:27:13 +0900

    linkkeeper: bump to 1.0.3

--- Casks/linkkeeper.rb
+++ Casks/linkkeeper.rb
@@ -1,8 +1,8 @@
 cask "linkkeeper" do
-  version "1.0.2"
-  sha256 "4d92e0ca0ba20a8261b5ef34033d5c64d1ff57e2d9cfc8b6db08449d189f0dae"
+  version "1.0.3"
+  sha256 "0ff3d9721e9b22d27a872c80f0801c8c6d78747e9770365a85b4b723a2c1c169"
 
-  url "https://github.com/shinyaohtani/link-keeper/releases/download/v1.0.2/LinkKeeper-#{version}.zip"
+  url "https://github.com/shinyaohtani/link-keeper/releases/download/v1.0.3/LinkKeeper-#{version}.zip"
   name "LinkKeeper"
   desc "macOS native bookmark manager utility"
   homepage "https://github.com/shinyaohtani/link-keeper"
